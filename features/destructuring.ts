98de1647b7d28cf43217647055b5f5522bc22c2e
hmachaao <hmachaao@gmail.com>
2019-02-02 17:25:09 +0100

    Project setup

---
+++ features/destructuring.ts
@@ -0,0 +1,23 @@
+import { Book } from '../interfaces';
+import * as util from '../lib/utilityFunctions';
+
+function PrintBokInfo(item: Book): void {
+    console.log(`${item.title} was authored by ${item.author}`)
+}
+
+function LogFavoriteBooks([book1, book2, ...others]: Book[]): void {
+    PrintBokInfo(book1);
+    PrintBokInfo(book2);
+    console.log(others);
+}
+
+export function ex1 () {
+    LogFavoriteBooks(util.GetAllBooks());
+}
+
+export function ex2 () {
+    let [book1]  = util.GetAllBooks();
+    let {title, author} = book1;
+    console.log(title);
+    console.log(author);
+}
